3554c9b5133548e0e9c9de549f606f14416b9987
Isabel Andrade <isabel4@stanford.edu>
2021-05-11 19:07:04 +0000

    Delete first row. Source right .csv file.

--- Isa_Cleaning_Code.R
+++ Isa_Cleaning_Code.R
@@ -1,24 +1,28 @@
----
+#---
 title: "Isa Cleaning Code"
 output: R
----
+#---
+
+df = read.csv("RLA_0507.csv")
+
+# Remove the first row
+df = df[-1, ]
+summary(df)
+
+# Turn treatment variables into dummy variables
 df$t_bipartisan <- ifelse(df$Treatment_group == "Bipartisan", 1, 0)
-table(df$t_bipartisan)
-
 df$t_percentage <- ifelse(df$Treatment_group == "RL_percentage", 1, 0)
-table(df$t_percentage)
-
 df$t_handcount <- ifelse(df$Treatment_group == "Handcount", 1, 0)
-table(df$t_handcount)
-
 df$t_losers <- ifelse(df$Treatment_group == "Loser", 1, 0)
-table(df$t_losers)
-
 df$t_soup <- ifelse(df$Treatment_group == "Soup", 1, 0)
-table(df$t_soup)
-
 df$t_local <- ifelse(df$Treatment_group == "Local", 1, 0)
-table(df$t_local)
-
 df$t_control <- ifelse(df$Treatment_group == "Control", 1, 0)
+
+# Number of observations in each treatment condition
+table(df$t_bipartisan)
+table(df$t_percentage)
+table(df$t_handcount)
+table(df$t_losers)
+table(df$t_soup)
+table(df$t_local)
 table(df$t_control)
